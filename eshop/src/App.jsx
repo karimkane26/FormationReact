504e63a34a3e057bda58521d77f8efdd9ef2a8e6
karimkane26 <contact@lucideve.com>
2026-09-04 16:25:19 +0000

    eshop

---
+++ eshop/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+     <div className="heeder_logo">
+
+     </div>
+
+     <div className="header_search"></div>
+
+     <div className="header_nav">
+      <div className="nav_item">
+         <span className="nav_itemLineOne">Hello Guest</span>
+      <span className='nav_itemLineTwo'>Sign In</span>
+      </div>
+
+      <div className="nav_item">
+         <span className="nav_itemLineOne">Hello Guest</span>
+      <span className='nav_itemLineTwo'>Sign In</span>
+      </div>
+
+      <div className="nav_item">
+         <span className="nav_itemLineOne">Hello Guest</span>
+      <span className='nav_itemLineTwo'>Sign In</span>
+      </div>
+     
+     </div>
+
+    </div>
+  )
+}
+
+export default App
